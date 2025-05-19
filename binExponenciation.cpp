14bbf41253448e8f867a4c9995e8805da8c1daeb
Soccho27 <137980333+Soccho27@users.noreply.github.com>
2025-05-19 06:01:10 +0600

    Create binExponenciation.cpp

---
+++ binExponenciation.cpp
@@ -0,0 +1,11 @@
+int binPow(int a, int b, int mod){
+    int res = 1;
+    while(b > 0){
+        if(b & 1){
+            res = (res * a) % mod;
+        }
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
